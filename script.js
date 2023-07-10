2ce0b1393401d17a91afaad0d2d383cb7ec92268
AliHamza8 <alihamza022bb@gmail.com>
2023-07-10 14:55:52 +0500

    commit

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
 const productLink = document.querySelector('.drop-effect')
 const dropDown = document.querySelector('.drop-down')
+const dropDownPro = document.querySelector('.drop-down-pro')
+const filter = document.querySelector('.filter')
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
-// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-//     hamburger.classList.remove("active")
-//     navMenu.classList.remove("active")
-// }))
 
 productLink.addEventListener("click", () => {
     productLink.classList.toggle("active")
     dropDown.classList.toggle("active")
+})
+
+filter.addEventListener("click", () => {
+    filter.classList.toggle("active")
+    dropDownPro.classList.toggle("active")   
 })
