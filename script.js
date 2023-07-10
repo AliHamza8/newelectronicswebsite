8a0ead480edec0c0445fce80ae62c6aa70b42a93
AliHamza8 <alihamza022bb@gmail.com>
2023-07-10 12:46:07 +0500

    added new file and dropdown menu

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
+const productLink = document.querySelector('.drop-effect')
+const dropDown = document.querySelector('.drop-down')
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     hamburger.classList.remove("active")
+//     navMenu.classList.remove("active")
+// }))
+
+productLink.addEventListener("click", () => {
+    productLink.classList.toggle("active")
+    dropDown.classList.toggle("active")
+})
